c6fcd81cf040ffad318f35a63462d239f8bc5b13
Margaret <wanjirundirangu43@gmail.com>
2022-06-04 05:09:51 +0300

    commit tree 6707bed892504baf8eb098e4039328cc27863e2d

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-function introduction(Aki) {
-  return (`Hi, my name is ${Aki}.`);
+function introduction(name) {
+  return (`Hi, my name is ${name}.`);
+
 }
-function introduction(Samip) {
-  return (`Hi, my name is ${Samip}.`);
+//function introduction(Samip) {
+ 
+//}
+function introductionWithLanguage(name, language) {
+  return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
 }
-function introductionWithLanguage(Aki, Emberjs) {
-  return (`Hi, my name is ${Aki} and I am learning to program in ${Emberjs}.`);
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+  return (`Hi, my name is ${name}} and I am learning to program in ${language}.`);
 }
-function introductionWithLanguageOptional(Gracie, language = "JavaScript") {
-  console.log (`Hi, my name is ${Gracie} and I am learning to program in ${language}.`);
-}
-function introductionWithLanguageOptional(Gracie, Python = "JavaScript") {
-  return (`Hi, my name is ${Gracie} and I am learning to program in ${Python}.`);
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+  return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
 }
